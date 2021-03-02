d0123e4cf3b80afab539e499ecc8ef3f6178baaf
Daniel Sasse <dsasse07@gmail.com>
2021-03-02 15:41:51 -0500

    Done

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,11 +1,22 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, useRouteMatch } from "react-router-dom";
 import MoviesList from "./MoviesList";
+import MovieShow from './MovieShow'
 
 function MoviesPage({ movies }) {
+
+  const match = useRouteMatch()
+
   return (
     <div>
       <MoviesList movies={movies} />
+      {/* This first route will only show when no movie is yet selected*/}
+      <Route exact path='/movies'>
+        <h3>Choose a movie from the list above</h3>
+      </Route>
+      <Route exact path={`${match.url}/:movieId`} >
+        <MovieShow movies={movies} />
+      </Route>
     </div>
   );
 }
